4bd9ef2168147b080ccd52939d6f817afe756996
Mykhailo <voloshenkomykhailo@gmail.com>
2026-04-11 17:33:29 +0300

    Bump API version to 1.2.3

--- src/docs/docs.go
+++ src/docs/docs.go
@@ -755,7 +755,7 @@ const docTemplate = `{
 
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
-	Version:          "1.2.2",
+	Version:          "1.2.3",
 	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
